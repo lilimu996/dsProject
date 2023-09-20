1604a51a291c55599bc31ae455edc53a0112b861
[lilimu996] <[junhaolics@foxmail.com]>
2023-09-20 21:31:36 +0800

    配置main函数

--- SmartHome_RTOS/App/main.c
+++ SmartHome_RTOS/App/main.c
@@ -1,8 +1,16 @@
 #include "main.h"
+#include "sys.h"
+
 
 int main(void)
 {
-
+	HAL_Init();
+	SystemClock_Config();
+	
+	
+	
+	
+	return 0;
 
 }
 
---
+++ SmartHome_RTOS/HAL/SYSTEM/sys/sys.h
@@ -0,0 +1,6 @@
+#ifndef __SYS_H
+#define __SYS_H
+
+
+void SystemClock_Config(void);
+#endif

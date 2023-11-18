6eff6bc9c9bccb39071d5dbc82fed78df2b9b597
lilimu996 <junhaolics@foxmail.com>
2023-11-18 20:16:43 +0800

    STM32F103zet6 HAL工程模版

---
+++ STM32_Template_F103/STM32_Driver/Main/main.h
@@ -0,0 +1,18 @@
+/*  Copyright (s) 2019 深圳百问网科技有限公司
+ *  All rights reserved
+ * 
+ * 文件名称：main.h
+ * 摘要：
+ *  
+ * 修改历史     版本号       Author       修改内容
+ *--------------------------------------------------
+ * 2020.6.6      v01        百问科技      创建文件
+ *--------------------------------------------------
+*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_clk.h"
+
+#endif
